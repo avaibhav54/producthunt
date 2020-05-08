f6e28a5c3a9777fa69412522191c0be12c85c15d
vaibhav agarwal <agarwalvaibhav041@gmail.com>
2020-05-08 12:22:57 +0530

    test heroku

--- products/views.py
+++ products/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 def home(request):
-    return render(request,'products/home.html')
+    products=Product.objects
+    return render(request,'products/home.html',{'products':products})
+@login_required
+def create(request):
+    if request.method=='POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product=Product()
+            product.title=request.POST['title']
+            product.body=request.POST['body']
+            product.icon=request.FILES['icon']
+            product.icon=request.FILES['image']
+            product.pub_date=timezone.datetime.now()
+            product.votes_total=1
+            product.hunter=request.user
+            product.save()
+            return redirect('home')
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url=request.POST['url']
+            else:
+                product.url='http://'+request.POST['url']
+
+        else:
+            return render(request,'products/create.html',{'error':'all fields required'})
+
+    else:
+        return render(request,'products/create.html')
